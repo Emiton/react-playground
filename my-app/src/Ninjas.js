4374a1520dc9cc14b67b4cbf13fc39a30f6d2a99
emiton <emiton_alves@my.uri.edu>
2018-09-18 19:24:13 -0400

    Redid all of the files because page wont load, although code is the same as the tutorial

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 const Ninjas = ({ninjas}) => {
-    //console.log(this.props);
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
-        )
-    })
-    return(
-        <div className="ninja-list">
-            { ninjaList }
-        </div> 
+
+  // const { ninjas } = this.props;
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
+      </div>
     )
+  });
+
+  return (
+    <div className="ninja-list">
+      { ninjaList }
+    </div>
+  );
+
 }
 
 export default Ninjas
